2f6862a391a8b1c5a836b062110dceacd0513cce
victorinojohnmark <victorinojohnmark@gmail.com>
2024-10-15 16:05:17 +0800

    Models, migration, factories and seeders

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+            ]
+        ];
+
+        User::insert($users);
+    }
+}
